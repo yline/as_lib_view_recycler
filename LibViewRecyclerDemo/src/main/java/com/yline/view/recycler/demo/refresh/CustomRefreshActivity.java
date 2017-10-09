ca5a4d390ee60cacd9f6b65de8ba63e0071dae00
yline <957339173@qq.com>
2017-10-09 10:11:41 +0800

    this:增加 头部刷新、底部下拉功能，暂时试用 next:多种类型

--- LibViewRecyclerDemo/src/main/java/com/yline/view/recycler/demo/refresh/CustomRefreshActivity.java
+++ LibViewRecyclerDemo/src/main/java/com/yline/view/recycler/demo/refresh/CustomRefreshActivity.java
@@ -4,16 +4,116 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.yline.base.BaseAppCompatActivity;
+import com.yline.test.StrConstant;
 import com.yline.view.recycler.demo.R;
+import com.yline.view.recycler.refresh.SimpleRefreshAdapter;
+import com.yline.view.recycler.refresh.SuperSwipeRefreshLayout;
+import com.yline.view.recycler.simple.SimpleHeadFootRecyclerAdapter;
+
+import java.util.Random;
 
 public class CustomRefreshActivity extends BaseAppCompatActivity {
+	private SuperSwipeRefreshLayout swipeRefreshLayout;
+
+	private RecyclerView recyclerView;
+
+	private SimpleHeadFootRecyclerAdapter recyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_refresh);
+
+		swipeRefreshLayout = (SuperSwipeRefreshLayout) findViewById(R.id.super_swipe_refresh);
+		recyclerView = (RecyclerView) findViewById(R.id.recycler_super_swipe);
+		recyclerAdapter = new SimpleHeadFootRecyclerAdapter();
+
+		swipeRefreshLayout.setRefreshAdapter(new SimpleRefreshAdapter(this)
+		{
+			// 动画下拉时，距离变动
+			@Override
+			protected void onCreating(float dragDistance, float targetDistance)
+			{
+				super.onCreating(dragDistance, targetDistance);
+			}
+
+			// 传递你需要定制的view;
+			@NonNull
+			@Override
+			protected View getView(Context context)
+			{
+				return super.getView(context);
+			}
+
+			// 正在刷新时，执行的操作
+			@Override
+			protected void onAnimate()
+			{
+				super.onAnimate();
+
+				new Handler().postDelayed(new Runnable()
+				{
+
+					@Override
+					public void run()
+					{
+						swipeRefreshLayout.setRefreshing(false);
+						// 更新recyclerView
+						recyclerAdapter.add(0, "Refresh Item + " + new Random().nextInt(300), true);
+						recyclerView.scrollToPosition(0);
+					}
+				}, 4000);
+			}
+
+			// 设置是否在表面；true:子View随下拉而一起滑动； false:子view不下滑，只是浮在上面
+			@Override
+			public boolean isTargetScroll()
+			{
+				return super.isTargetScroll();
+			}
+
+			// 返回背景颜色
+			@Override
+			public int getBackgroundResource()
+			{
+				return super.getBackgroundResource();
+			}
+		});
+
+		// 支持自定义（view）
+		swipeRefreshLayout.setLoadAdapter(null);
+		/*swipeRefreshLayout.setOnLoadListener(new ViewSwipeRefreshLayout.OnSwipeListener()
+		{
+			@Override
+			public void onAnimate()
+			{
+				new Handler().postDelayed(new Runnable()
+				{
+
+					@Override
+					public void run()
+					{
+						swipeRefreshLayout.setLoadMore(false);
+						// 更新recyclerView
+						int itemNumber = recyclerAdapter.getItemCount();
+						recyclerAdapter.add(itemNumber, "Loaded Item + " + new Random().nextInt(300));
+						recyclerView.scrollToPosition(itemNumber);
+					}
+				}, 4000);
+			}
+		});*/
+
+		recyclerView.setLayoutManager(new LinearLayoutManager(this));
+		recyclerView.setAdapter(recyclerAdapter);
+
+		recyclerAdapter.setDataList(StrConstant.getListSeven(20), true);
     }
     
     public static void launcher(Context context)
--- LibViewRecyclerDemo/src/main/java/com/yline/view/recycler/demo/refresh/SimpleRefreshActivity.java
+++ LibViewRecyclerDemo/src/main/java/com/yline/view/recycler/demo/refresh/SimpleRefreshActivity.java
@@ -4,28 +4,78 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.yline.base.BaseAppCompatActivity;
+import com.yline.test.StrConstant;
 import com.yline.view.recycler.demo.R;
+import com.yline.view.recycler.refresh.SuperSwipeRefreshLayout;
+import com.yline.view.recycler.simple.SimpleHeadFootRecyclerAdapter;
+
+import java.util.Random;
 
 public class SimpleRefreshActivity extends BaseAppCompatActivity {
+    private SuperSwipeRefreshLayout swipeRefreshLayout;
+
+    private RecyclerView recyclerView;
+
+    private SimpleHeadFootRecyclerAdapter recyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_refresh);
+
+        swipeRefreshLayout = (SuperSwipeRefreshLayout) findViewById(R.id.super_swipe_refresh);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_super_swipe);
+        recyclerAdapter = new SimpleHeadFootRecyclerAdapter();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(recyclerAdapter);
+
+        swipeRefreshLayout.setOnRefreshListener(new SuperSwipeRefreshLayout.OnSwipeListener() {
+            @Override
+            public void onAnimate() {
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        swipeRefreshLayout.setRefreshing(false);
+                        // 更新recyclerView
+                        recyclerAdapter.add(0, "Refresh Item + " + new Random().nextInt(300), true);
+                        recyclerView.scrollToPosition(0);
+                    }
+                }, 4000);
+            }
+        });
+        swipeRefreshLayout.setOnLoadListener(new SuperSwipeRefreshLayout.OnSwipeListener() {
+            @Override
+            public void onAnimate() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeRefreshLayout.setLoadMore(false);
+                        // 更新recyclerView
+                        int itemNumber = recyclerAdapter.getItemCount();
+                        recyclerAdapter.add(itemNumber, "Loaded Item + " + new Random().nextInt(300), true);
+                        recyclerView.scrollToPosition(itemNumber);
+                    }
+                }, 4000);
+            }
+        });
+
+        recyclerAdapter.setDataList(StrConstant.getListSeven(20), true);
     }
 
-    public static void launcher(Context context)
-    {
-    	if (null != context)
-    	{
-    		Intent intent = new Intent(context, SimpleRefreshActivity.class);
-    		if (!(context instanceof Activity))
-    		{
-    			intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    		}
-    		context.startActivity(intent);
-    	}
+    public static void launcher(Context context) {
+        if (null != context) {
+            Intent intent = new Intent(context, SimpleRefreshActivity.class);
+            if (!(context instanceof Activity)) {
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            }
+            context.startActivity(intent);
+        }
     }
 }
